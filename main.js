d49b09b2b9ee53ce533f0c306e9af5633442ddc4
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-08-23 02:18:44 +0600

    update change

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 document.getElementById('addMoneyBtn').addEventListener('click', function (e) {
     e.preventDefault()
-    
+    const bank = document.getElementById('bank').value
+const accountNumber = document.getElementById('accountNumber').value
+const addMoney = parseInt(document.getElementById('addMoney').value)
+const pin = parseInt(document.getElementById('pin').value)
+
 })
