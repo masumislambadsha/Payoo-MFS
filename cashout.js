7992e4da2a0da5e75663ff8da8c13b2d3e1015b6
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-08-23 03:43:33 +0600

    update change

---
+++ cashout.js
@@ -0,0 +1,34 @@
+const AgentNumber = 19296866940
+const validAgentNumber = parseInt(AgentNumber)
+const validPin = 1234
+document.getElementById('cashoutMoneyBtn').addEventListener('click', function (e) {
+    e.preventDefault()
+const agentNumber = document.getElementById('agentNumber').value
+const removeMoney = parseInt(document.getElementById('removeMoney').value)
+const cashoutPin = parseInt(document.getElementById('cashoutPin').value)
+
+    const currentBalance = parseInt(document.getElementById('current-balance').innerText)
+
+    if ( agentNumber !=validAgentNumber ) {
+        document.getElementById('accountNumberError').innerText ='*Please Provide A Valid Account Number*'
+        return
+    }
+    if (removeMoney < 0) {
+        document.getElementById('amountError').innerText ='*Please Provide A Valid Amount*'
+        return
+    }
+    if (cashoutPin != validPin) {
+        document.getElementById('pinError').innerText ='*Please Provide Correct PIN*'
+        return
+    }
+
+    const newBalance = currentBalance - removeMoney
+    document.getElementById('current-balance').innerText = newBalance
+    document.getElementById('agentNumber').value = ''
+    document.getElementById('removeMoney').value = ''
+    document.getElementById('cashoutPin').value = ''
+    document.getElementById('cashoutPinError').innerText = ''
+    document.getElementById('agentNumberError').innerText = ''
+    document.getElementById('removeAmountError').innerText = ''
+    console.log(newBalance)
+})
