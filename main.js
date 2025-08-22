f6b4632460ced4d2420ac46bdb18903fa005d380
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-08-23 02:21:21 +0600

    update change

--- main.js
+++ main.js
@@ -5,4 +5,7 @@ const accountNumber = document.getElementById('accountNumber').value
 const addMoney = parseInt(document.getElementById('addMoney').value)
 const pin = parseInt(document.getElementById('pin').value)
 
+    const currentBalance = parseInt(document.getElementById('current-balance').innerText)
+
+
 })
